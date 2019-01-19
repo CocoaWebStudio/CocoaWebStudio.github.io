eb6762da1e61ee22598c86446a767adb2a2f5d3b
gabriela <gaby@lliguin.com>
2019-01-19 16:00:37 -0500

    add navmenu to coustomer

---
+++ pages/customers.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .customer
+    navMenu
+    .container
+     h1 Customers Smarensol
+</template>
+<script>
+import navMenu from '~/components/nav/navMenu.vue'
+
+export default {
+  name: 'aboutUs',
+  components: {
+    navMenu
+  }
+}
+</script>
