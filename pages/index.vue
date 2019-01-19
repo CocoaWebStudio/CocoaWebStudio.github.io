567cb3c95d9b8eeaf1910071485fdf6a2cf79b17
gabriela <gaby@lliguin.com>
2019-01-19 17:42:26 -0500

    edit index

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,11 @@
   .home
     navMenu
     slider
-    .row
-      h1.title Welcome to Smarensol
-      p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor elit vel augue gravida, et ultricies massa scelerisque. Vivamus cursus semper arcu at sollicitudin. Pellentesque imperdiet metus eu egestas posuere. Nulla odio ligula, congue vel posuere ut, accumsan quis lectus. Aenean velit turpis, iaculis ut suscipit at, luctus eget dolor. Curabitur sed magna vel magna rutrum accumsan. Fusce ultrices felis mauris, vitae porttitor arcu auctor quis. Fusce eget libero laoreet, vestibulum ipsum et, tempus lorem. Nam lacus eros, convallis ornare rhoncus non, aliquet et sem. In at efficitur risus, vitae tempor diam. Donec volutpat nec sem non iaculis. Quisque fringilla eros ligula, in rutrum libero efficitur vel. Cras luctus cursus commodo. Ut euismod, odio vitae suscipit tristique, lorem turpis ultrices purus, at iaculis dolor turpis in tortor. Aliquam laoreet nunc ante, ultricies feugiat nulla congue eu.
-      p Etiam ut eros non turpis imperdiet rhoncus. Vestibulum viverra dolor id libero sollicitudin faucibus. Curabitur vehicula sollicitudin est sed suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam id felis lacus. Aenean hendrerit ex nec aliquam auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed massa tellus. Aliquam fringilla nisl risus, in ultrices sapien porttitor id.
+    .container
+      .row
+        h1.title Welcome to Smarensol
+        p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor elit vel augue gravida, et ultricies massa scelerisque. Vivamus cursus semper arcu at sollicitudin. Pellentesque imperdiet metus eu egestas posuere. Nulla odio ligula, congue vel posuere ut, accumsan quis lectus. Aenean velit turpis, iaculis ut suscipit at, luctus eget dolor. Curabitur sed magna vel magna rutrum accumsan. Fusce ultrices felis mauris, vitae porttitor arcu auctor quis. Fusce eget libero laoreet, vestibulum ipsum et, tempus lorem. Nam lacus eros, convallis ornare rhoncus non, aliquet et sem. In at efficitur risus, vitae tempor diam. Donec volutpat nec sem non iaculis. Quisque fringilla eros ligula, in rutrum libero efficitur vel. Cras luctus cursus commodo. Ut euismod, odio vitae suscipit tristique, lorem turpis ultrices purus, at iaculis dolor turpis in tortor. Aliquam laoreet nunc ante, ultricies feugiat nulla congue eu.
+        p Etiam ut eros non turpis imperdiet rhoncus. Vestibulum viverra dolor id libero sollicitudin faucibus. Curabitur vehicula sollicitudin est sed suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam id felis lacus. Aenean hendrerit ex nec aliquam auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed massa tellus. Aliquam fringilla nisl risus, in ultrices sapien porttitor id.
 </template>
 
 <script>
@@ -23,16 +24,8 @@ export default {
 </script>
 
 <style>
-.row {
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 .container {
   margin: 0 auto;
-  height: 60vh;
   display: flex;
   justify-content: center;
   align-items: center;
