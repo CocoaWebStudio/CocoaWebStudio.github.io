decea26e8bc2f673c4be92bf8ae259d3124098b7
gabriela <gaby@lliguin.com>
2019-01-19 19:16:59 -0500

    add submenu routers to products

---
+++ pages/productsService.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  navMenu
+</template>
+<script>
+import navMenu from '~/components/nav/navMenu.vue'
+
+export default {
+  name: 'products',
+  components: {
+    navMenu
+  }
+}
+</script>
