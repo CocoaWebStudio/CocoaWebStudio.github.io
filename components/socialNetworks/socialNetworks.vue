96f41c6347a00bc65645a3c61a50c07ac5df1414
gabriela <gaby@lliguin.com>
2019-01-21 17:39:08 -0500

    fix social network component

--- components/socialNetworks/socialNetworks.vue
+++ components/socialNetworks/socialNetworks.vue
@@ -3,7 +3,7 @@
     a(href="www.facebook.com")
       network(network="facebook")
         i.fa.fa-facebook Facebook &nbsp
-    a(href="www.twitter.com")
+    a(href="www.twiter.com")
       network(network="twitter")
         i.fa.fa-twitter Twitter
 </template>
