e00ab90355bbf2bb482a07f7fe4c31bccdfe3fd0
gabriela <gaby@lliguin.com>
2019-01-22 18:26:13 -0500

    edit contactform

--- components/contactForm/contactForm.vue
+++ components/contactForm/contactForm.vue
@@ -1,1 +1,29 @@
-<template src="./contactForm.pug"></template>
+<template src="./contactForm.pug" />
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        name: '',
+        telephone: '',
+        comentary: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(evt) {
+      evt.preventDefault()
+      /* Reset our form values */
+      this.form.email = ''
+      this.form.name = ''
+      this.form.telephone = ''
+      this.form.comentary = ''
+    }
+  }
+}
+</script>
