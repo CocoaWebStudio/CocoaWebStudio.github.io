37552dfc26c33370d1e495bb1d2c0e12a8c79932
gabriela <gaby@lliguin.com>
2019-01-23 11:50:02 -0500

    edit social networks

--- components/socialNetworks/socialNetworks.vue
+++ components/socialNetworks/socialNetworks.vue
@@ -1,9 +1,6 @@
 <template lang="pug">
   .div
-    a(href="www.facebook.com")
-      network(network="facebook")
-        i.fa.fa-facebook Facebook &nbsp
-    a(href="www.twiter.com")
-      network(network="twitter")
-        i.fa.fa-twitter Twitter
+    a(href="https://ec.linkedin.com/")
+      network(network="linkedin")
+        i.fa.fa-linkedin LinkedIn
 </template>
