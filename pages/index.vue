bc5ce6170694f436d48f275cce7a00e59941826c
gabriela <gaby@lliguin.com>
2019-01-23 10:58:04 -0500

    add brief welcome to index

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,6 @@
   .home
     navMenu
     slider
-    .container
-      .row
-        .col.text-center
-          h1.title Welcome to Smarensol
-      .row
-        .col
-          p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor elit vel augue gravida. Vivamus cursus semper arcu at sollicitudin. Pellentesque imperdiet metus eu egestas posuere. Nulla odio ligula, congue vel posuere ut, accumsan quis lectus. Aenean velit turpis, iaculis ut suscipit at, luctus eget dolor. Curabitur sed magna vel magna rutrum accumsan. Fusce ultrices felis mauris, vitae porttitor arcu auctor quis. Fusce eget libero laoreet, vestibulum ipsum et, tempus lorem. Nam lacus eros, convallis ornare rhoncus non, aliquet et sem. In at efficitur risus, vitae tempor diam. Donec volutpat nec sem non iaculis. Quisque fringilla eros ligula, in rutrum libero efficitur vel. Cras luctus cursus commodo. Ut euismod, odio vitae suscipit tristique
-          p Etiam ut eros non turpis imperdiet rhoncus. Vestibulum viverra dolor id libero sollicitudin faucibus. Curabitur vehicula sollicitudin est sed suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam id felis lacus. Aenean hendrerit ex nec aliquam auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed massa tellus. Aliquam fringilla nisl risus, in ultrices sapien porttitor id.
-      br
     .container-fluid
       .row
         b-col(align="right") follow us
