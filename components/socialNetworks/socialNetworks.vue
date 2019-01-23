6b2b83c72d6293fa6f475294e1fbc18bdd7fd7fd
gabriela <gaby@lliguin.com>
2019-01-23 17:44:45 -0500

    fix social Networks component

--- components/socialNetworks/socialNetworks.vue
+++ components/socialNetworks/socialNetworks.vue
@@ -1,9 +1,7 @@
 <template lang="pug">
   .div
     a(href="https://ec.linkedin.com/")
-      network(network="linkedin")
-        i.fa.fa-linkedin LinkedIn
+      i.fa.fa-linkedin LinkedIn
     a(href="https://ec.linkedin.com/")
-      network Download Brochure
-        i.fa.fa
+      i.fa.fa Download Brochure
 </template>
