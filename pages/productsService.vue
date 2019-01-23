b07c893b30676621cd427c1b068dc16e1f7b21e5
gabriela <gaby@lliguin.com>
2019-01-22 19:38:23 -0500

    add gallery component

--- pages/productsService.vue
+++ pages/productsService.vue
@@ -1,13 +1,23 @@
 <template lang="pug">
-  navMenu
+  .products
+    navMenu
+    br
+    .row
+      .col.text-center
+         h1.title Products and Services
+    br
+    .container
+      gallery
 </template>
 <script>
 import navMenu from '~/components/nav/navMenu.vue'
+import gallery from '~/components/gallery/gallery.vue'
 
 export default {
   name: 'products',
   components: {
-    navMenu
+    navMenu,
+    gallery
   }
 }
 </script>
