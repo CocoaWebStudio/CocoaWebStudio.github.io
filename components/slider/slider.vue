7e04d28c0a79310f2721389583f84a3ae34ee4ac
gabriela <gaby@lliguin.com>
2019-01-23 20:09:26 -0500

    fix slider component

--- components/slider/slider.vue
+++ components/slider/slider.vue
@@ -1,1 +1,18 @@
 <template src="./slider.pug" />
+<script>
+import smarensol from '~/assets/images/smarensol.jpg'
+import energy from '~/assets/images/energy.jpg'
+import industry from '~/assets/images/industry.jpg'
+import oil from '~/assets/images/oil.jpg'
+
+export default {
+  data() {
+    return {
+      smarensol: smarensol,
+      energy: energy,
+      industry: industry,
+      oil: oil
+    }
+  }
+}
+</script>
