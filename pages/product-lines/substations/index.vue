e9ccf665e5cf12b3281933510feabe99bc6713c4
gabriela <gaby@lliguin.com>
2019-01-24 17:21:26 -0500

    add component gallery product lines sub menus

--- pages/product-lines/substations/index.vue
+++ pages/product-lines/substations/index.vue
@@ -5,14 +5,19 @@
       .row
         .col.text-center
           h1.title Substations Smarensol
+          br
+      .jumbotron
+        gallery
 </template>
 <script>
 import navMenu from '~/components/nav/navMenu.vue'
+import gallery from '~/components/gallery/gallery.vue'
 
 export default {
   name: 'Substations',
   components: {
-    navMenu
+    navMenu,
+    gallery
   }
 }
 </script>
--- pages/product-lines/transmission-lines/_transmission.vue
+++ pages/product-lines/transmission-lines/_transmission.vue
@@ -5,14 +5,19 @@
       .row
         .col.text-center
           h1.title Transmission Lines Smarensol
+          br
+      .jumbotron
+       gallery
 </template>
 <script>
 import navMenu from '~/components/nav/navMenu.vue'
+import gallery from '~/components/gallery/gallery.vue'
 
 export default {
   name: 'Transmission',
   components: {
-    navMenu
+    navMenu,
+    gallery
   }
 }
 </script>
