c1dbfdc3880bc0ea27c7b09a22570fa1cb3e1225
gabriela <gaby@lliguin.com>
2019-01-28 17:56:33 -0500

    Revert "create routers page"
    This reverts commit 8101b6359710ae4fcb15bef37b427908fee67cf7.

--- i18nRouter.js
+++ i18nRouter.js
@@ -11,30 +11,6 @@ module.exports= {
     'customers': {
       en: '/customers',
       es: '/clientes'
-    },
-    'index': {
-      en: '/home',
-      es: '/inicio'
-    },
-    'product-lines': {
-      en: '/product-lines',
-      es: '/lineas-productos'
-    },
-    'product-lines/distribution': {
-      en: '/distribution',
-      es: '/distribucion'
-    },
-    'product-lines/substations/index': {
-      en: '/substations',
-      es: '/subestaciones'
-    },
-    'product-lines/transmission-lines/_transmission': {
-      en: '/transmission-lines',
-      es: '/lineas-transmision'
-    },
-    'product-lines/_industrial/insdustrial': {
-      en: '/industrial',
-      es: '/industrial'
     }
   }
 }
