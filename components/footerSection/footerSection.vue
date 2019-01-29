1c020a8f98c5773578c2a7378f91b4ab03cb9f3b
el Pablo <pablo@lliguin.com>
2019-01-29 11:35:04 -0500

    add footerSection

---
+++ components/footerSection/footerSection.vue
@@ -0,0 +1,14 @@
+<template src="./footerSection.pug" />
+
+<script>
+import socialNetworks from '~/components/socialNetworks/socialNetworks.vue'
+
+export default {
+  name: 'footerSection',
+  components: {
+    socialNetworks
+  }
+}
+</script>
+
+<style src="./footerSection.scss" lang="scss" scoped />
