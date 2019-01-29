6e6d6e0a6a676da7c020ef2e0638d6ecbd18e2e3
el Pablo <pablo@lliguin.com>
2019-01-29 12:21:37 -0500

    add social icons

--- components/socialNetworks/socialNetworks.vue
+++ components/socialNetworks/socialNetworks.vue
@@ -1,7 +1,31 @@
 <template lang="pug">
-  .div
-    a(href="https://ec.linkedin.com/")
-      i.fa.fa-linkedin LinkedIn
-    a(href="https://ec.linkedin.com/")
-      i.fa.fa Download Brochure
+  .row
+    .col
+      a.text-light(href="https://ec.linkedin.com/")
+        facebook-icon
+    .col
+      a.text-light(href="https://ec.linkedin.com/")
+        TwitterIcon
+    .col
+      a.text-light(href="https://ec.linkedin.com/")
+        LinkedinIcon
+    .col
+      a.text-light(href="mailto:ventas@smarensol.com")
+        MailIcon
+    .col
+      a.text-light(href="whatsapp://tel:3216541234")
+        PhoneIcon
 </template>
+<script>
+  import { FacebookIcon,  TwitterIcon, LinkedinIcon, MailIcon, PhoneIcon } from 'vue-feather-icons'
+
+  export default {
+    components: {
+      FacebookIcon,
+      TwitterIcon,
+      LinkedinIcon,
+      MailIcon,
+      PhoneIcon
+    }
+  }
+</script>
