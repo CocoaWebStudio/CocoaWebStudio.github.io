2715daf6e70474555ed8f375ba1ed462f428f95d
gabriela <gaby@lliguin.com>
2019-01-28 19:56:26 -0500

    fix translations location to contact us page

--- components/location/location.vue
+++ components/location/location.vue
@@ -1,7 +1,8 @@
+<i18n src="./location.json"></i18n>
 <template lang="pug">
   .container
     .row
       .col.text-center
-        h1.subtitle Location
+        h1.subtitle {{ $t('location') }}
 </template>
 <style src="./location.scss" lang="scss" scoped />
