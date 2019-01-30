dc1ccb12f9cea8826d30351bcbef57221b44d1e2
el Pablo <pablo@lliguin.com>
2019-01-30 08:31:58 -0500

    add right links

--- components/socialNetworks/socialNetworks.vue
+++ components/socialNetworks/socialNetworks.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
   .row
     .col.py-3
-      a.text-light(href="https://ec.linkedin.com/", target="_blank", rel="noopener noreferrer")
+      a.text-light(
+        href="//www.facebook.com/Smarensol-SA-374476870003912/",
+        target="_blank",
+        rel="noopener noreferrer"
+      )
         facebook-icon
     .col.py-3
-      a.text-light(href="https://ec.linkedin.com/", target="_blank", rel="noopener noreferrer")
-        TwitterIcon
-    .col.py-3
-      a.text-light(href="https://ec.linkedin.com/", target="_blank", rel="noopener noreferrer")
+      a.text-light(
+        href="linkedin.com/company/smarensol-s-a",
+        target="_blank",
+        rel="noopener noreferrer"
+      )
         LinkedinIcon
     .col.py-3
       a.text-light(href="mailto:ventas@smarensol.com", target="_blank")
@@ -18,12 +23,11 @@
         span  (+593) 994138795
 </template>
 <script>
-  import { FacebookIcon,  TwitterIcon, LinkedinIcon, MailIcon, PhoneIcon } from 'vue-feather-icons'
+  import { FacebookIcon, LinkedinIcon, MailIcon, PhoneIcon } from 'vue-feather-icons'
 
   export default {
     components: {
       FacebookIcon,
-      TwitterIcon,
       LinkedinIcon,
       MailIcon,
       PhoneIcon
