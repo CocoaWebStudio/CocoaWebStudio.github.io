21a665d0f6c29a837fb8d4d712c7d71cb92018d8
gabriela <gaby@lliguin.com>
2019-01-30 19:21:56 -0500

    add allies image to index
    test reusable component imageSection

---
+++ components/imageSection/imageSection.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  b-container(class="p-4 bg-ligh")
+    b-row
+      //b-col
+        b-img(v-for="imageSection in imageSections"
+          :key="imageSection.id"
+          :img-src="imageSection.url"
+          )
+      b-col
+        b-img(thumbnail fluid src="/images/allies/delta.jpg" alt="Thumbnail")
+      b-col
+        b-img(thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Thumbnail")
+      b-col
+        b-img(thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Thumbnail")
+</template>
+
+<script>
+export default {
+
+  props: {
+    imageSections: {
+      type: Array,
+      required: true
+     }
+   }
+ }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,14 +10,17 @@
       .row
         .col.text-center
           h1.allies {{ $t('allies-us')}}
+    imageSection(:imageSections="imageSections")
 </template>
 
 <script>
 import slider from '~/components/slider/slider.vue'
+import imageSection from '~/components/imageSection/imageSection.vue'
 
 export default {
   components: {
-    slider
+    slider,
+    imageSection
   },
   data() {
     return {
@@ -26,6 +29,9 @@ export default {
         {id:1,title: 'energy' , url:'/images/energy1.jpg'},
         {id:2,title: 'industry' , url:'/images/energy4.jpg'},
         {id:3,title: 'oil' , url:'/images/energy3.jpg'}
+      ],
+      imageSections: [
+        {id:0 , url:'/images/allies/delta.jpg'}
       ]
     }
   }
