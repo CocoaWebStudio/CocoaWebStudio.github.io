d6447c59ef260dc4c3f959ab52c43fda7bc7518b
gabriela <gaby@lliguin.com>
2019-01-30 20:05:14 -0500

    add location image to location component

--- components/location/location.vue
+++ components/location/location.vue
@@ -4,5 +4,9 @@
     .row
       .col.text-center
         h1.subtitle {{ $t('location') }}
+    .row
+      .col.text-center
+        a(href="#")
+          b-img(thumbnail fluid src="/images/location.jpg" alt="Thumbnail")
 </template>
 <style src="./location.scss" lang="scss" scoped />
