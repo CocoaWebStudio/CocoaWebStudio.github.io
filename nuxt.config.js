5c951b0f13d78bb2750238d5a1506114478f5f9d
el Pablo <pablo@lliguin.com>
2019-02-03 20:48:15 -0500

    configure more complex head

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,100 +1,93 @@
 const pkg = require('./package')
-const { pages } = require('./i18nRouter')
+const i18n = require('./i18nConfig')
 module.exports = {
-  mode: 'universal',
+	mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		meta: [{
+				charset: 'utf-8'
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{
+				name: 'robots',
+				content: 'index, follow'
+			}
+		],
+		noscript: [{
+			innerHTML: 'This website requires JavaScript.'
+		}],
+		link: [{
+			rel: 'icon',
+			type: 'image/x-icon',
+			href: '/favicon.ico'
+		}]
+	},
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#0002F5' },
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: {
+		color: '#0002F5'
+	},
 
-  /*
-  ** Global CSS
-  */
-  css: [
-    '@/assets/style/main.scss'
-  ],
+	/*
+	 ** Global CSS
+	 */
+	css: [
+		'@/assets/style/main.scss'
+	],
 
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [],
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [],
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    // Doc: https://bootstrap-vue.js.org/docs/
-    ['bootstrap-vue/nuxt', { css: false }],
-    '@nuxtjs/pwa',
-    ['nuxt-i18n', {
-      locales: [
-        { code: 'en', iso: 'en-US', file: 'en.js', name: 'English' },
-        { code: 'es', iso: 'es-EC', file: 'es.js', name: 'Español' }
-      ],
-      strategy: 'prefix_and_default',
-      defaultLocale: 'es',
-      parsePages: false,
-      pages,
-      lazy: true,
-      langDir: 'lang/',
-      detectBrowserLanguage: {
-        // If enabled, a cookie is set once a user has been redirected to his
-        // preferred language to prevent subsequent redirections
-        // Set to false to redirect every time
-        useCookie: true,
-        // Cookie name
-        cookieKey: 'i18n_redirected',
-        // Set to always redirect to value stored in the cookie, not just once
-        alwaysRedirect: true,
-        // If no locale for the browsers locale is a match, use this one as a fallback
-        fallbackLocale: 'es'
-      },
-      vueI18nLoader: true
-    }]
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		// Doc: https://bootstrap-vue.js.org/docs/
+		['bootstrap-vue/nuxt', {
+			css: false
+		}],
+		'@nuxtjs/pwa',
+		['nuxt-i18n', i18n]
+	],
+	/*
+	 ** Axios module configuration
+	 */
+	axios: {
+		// See https://github.com/nuxt-community/axios-module#options
+	},
 
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-          options : {
-            fix : true
-          }
-        })
-      }
-    }
-  }
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {
+			// Run ESLint on save
+			if (ctx.isDev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/,
+					options: {
+						fix: true
+					}
+				})
+			}
+		}
+	}
 }
