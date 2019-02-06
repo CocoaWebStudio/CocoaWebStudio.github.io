9f630b3f92a9dfdc367f935d1de9f2f869ac614e
gabriela <gaby@lliguin.com>
2019-02-06 12:45:44 -0500

    fix background-color to about-us

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -1,10 +1,12 @@
 <i18n src="./content.json" ></i18n>
 <template lang="pug">
   .container-fluid.bg
+    br
     .row
       .col-md-6.offset-md-3.bg-color
         h1.title.bounce-enter-active {{ $t('about-us') }}
         p.blockquote {{ $t('description') }}
+    br
     .container
       .row
         .col-md-6.bg-color
@@ -13,6 +15,7 @@
         .col-md-6.bg-color
           h2.title.bounce-enter-active {{ $t('vision') }}
           p.blockquote {{ $t('vision-content') }}
+    br
     .row
       .col-md-6.offset-md-3.text-center.bg-color
         h2.title.bounce-enter-active  {{ $t('values') }}
