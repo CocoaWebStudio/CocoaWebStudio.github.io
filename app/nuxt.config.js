894eceff3045ab2af70647848db823f8e896ee63
Juan Pablo Aguilar-Lliguin <pablo.aguilar-lliguin@solutionstlm.com>
2019-02-23 08:56:56 -0500

    install vee-validate

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -48,7 +48,9 @@ module.exports = {
 	/*
 	 ** Plugins to load before mounting the App
 	 */
-	plugins: [],
+	plugins: [
+    { src: '~/plugins/vee-validate.js', ssr: false }
+  ],
 
 	/*
 	 ** Nuxt.js modules
