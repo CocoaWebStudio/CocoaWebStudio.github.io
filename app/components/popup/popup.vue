c9d5139f56d98a18b89a27ba2df755856a8d0e37
Juan Pablo Aguilar-Lliguin <pablo.aguilar-lliguin@solutionstlm.com>
2019-02-28 08:31:48 -0500

    validate popup form

--- app/components/popup/popup.vue
+++ app/components/popup/popup.vue
@@ -1,48 +1,32 @@
-<i18n src="./popup.json"></i18n>
-<template lang="pug">
-  div
-    b-modal(id="popup-wraper"
-      ref="modal"
-      title= "Smarensol Brochure"
-      @oks="handleOk"
-      @shown="clear"
-     )
-      form(@submit.stop.prevent="handleSubmit")
-        b-form-group(id="exampleInputGroup1"
-          label-for="exampleInput1"
-         ) {{ $t('email') }}
-          b-form-input(id="exampleInput1"
-            type="email"
-            v-model="email"
-            required
-          )
-        b-form-group(id="exampleInputGroup2"
-          label-for="exampleInput2"
-         ) {{ $t('name') }}
-          b-form-input(id="exampleInput2"
-            type="text"
-            v-model="name"
-            required
-          )
-</template>
+<i18n src="./popup.json" />
+<template src="./popup.pug" lang="pug" />
 <script>
+import VueRecaptcha from 'vue-recaptcha'
 export default {
   name: 'Popup',
+  components: {
+    VueRecaptcha
+  },
   data() {
     return {
-      name: '',
-      email: ''
+      form: {
+        name: '',
+        email: ''
+      }
     }
+  },
+  $_veeValidate: {
+    validator: 'new' // give me my own validator scope.
   },
   methods: {
     clear() {
-      this.name = ''
-      this.email = ''
+      this.form.name = ''
+      this.form.email = ''
     },
     handleOk(evt) {
       // Prevent modal from closing
       evt.preventDefault()
-      if (!this.name) {
+      if (!this.form.name) {
         alert('Please enter your name')
       } else {
         this.handleSubmit()
@@ -52,6 +36,27 @@ export default {
       this.names.push(this.name)
       this.clear()
       this.$refs.modal.hide()
+    },
+    onSubmit() {
+      this.$refs.recaptcha.execute()
+    },
+    onCaptchaVerified(recaptchaToken) {
+      this.status = 'submitting'
+      this.$refs.recaptcha.reset()
+      this.form.recaptcha = recaptchaToken
+      this.onSend()
+    },
+    validateState(ref) {
+      if (
+        this.veeFields[ref] &&
+        (this.veeFields[ref].dirty || this.veeFields[ref].validated)
+      ) {
+        return !this.errors.has(ref)
+      }
+      return null
+    },
+    onCaptchaExpired: function() {
+      this.$refs.recaptcha.reset()
     }
   }
 }
