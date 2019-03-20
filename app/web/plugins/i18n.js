d664817f86d09d743566273cef3874835d37c0eb
el Pablo <pablo@lliguin.com>
2019-03-20 16:38:34 -0400

    optimize vee-validate

--- app/web/plugins/i18n.js
+++ app/web/plugins/i18n.js
@@ -1,6 +1,6 @@
 import en from 'vee-validate/dist/locale/en'
 import es from 'vee-validate/dist/locale/es'
-import { Validator } from 'vee-validate'
+import { Validator } from 'vee-validate/dist/vee-validate.minimal.esm.js'
 export default function({ app }) {
   // Loading languages for Vee
 
--- app/web/plugins/vee-validate.js
+++ app/web/plugins/vee-validate.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
-import VeeValidate from 'vee-validate'
+import {
+  Validator,
+  install as VeeValidate
+} from 'vee-validate/dist/vee-validate.minimal.esm.js'
+import { required, min, max, email } from 'vee-validate/dist/rules.esm.js'
+
+// Add the rules you need.
+Validator.extend('required', required)
+Validator.extend('min', min)
+Validator.extend('max', max)
+Validator.extend('email', email)
 
 Vue.use(VeeValidate, {
   inject: false,
