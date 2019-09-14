c800351d3b0c9b2c0f5773bd6632073965170d16
gabriela <gaby@lliguin.com>
2019-09-13 20:35:07 -0500

    fix galleryHorizontal component

--- app/web/components/galleryHorizontal/galleryHorizontal.vue
+++ app/web/components/galleryHorizontal/galleryHorizontal.vue
@@ -1,15 +1,10 @@
 <template lang="pug" src="./galleryHorizontal.pug" />
 
 <script>
-import Popup from '~/components/popup/popup.vue'
-
 export default {
   name: 'GalleryHorizontal',
-  components: {
-    Popup
-  },
   props: {
-    elements: {
+    galleryHorizontals: {
       type: Array,
       required: true
     }
