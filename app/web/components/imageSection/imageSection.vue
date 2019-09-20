8fb00ea23a4baddb08b20266f2cf15666ddcde63
el Pablo <pablo@lliguin.com>
2019-09-20 13:17:26 -0400

    fix small bugs

--- app/web/components/imageSection/imageSection.vue
+++ app/web/components/imageSection/imageSection.vue
@@ -1,20 +1,18 @@
 <template lang="pug">
-  b-container.py-2.gb-light
-    b-row
-      b-col( v-for="image in imageSections"
-        :key="image.id",
-        sm="6",
-        md="3",
-        cols="6"
-
+  b-row.py-4(no-gutters)
+    b-col( v-for="image in imageSections"
+      :key="image.id",
+      sm="6",
+      md="2",
+      cols="6"
+    )
+      b-img(
+        :alt="image.name",
+        thumbnail,
+        fluid,
+        v-lazy="image.image",
+        height="100"
       )
-        b-img(
-          :alt="image.name",
-          thumbnail,
-          fluid,
-          v-lazy="image.image",
-          height="100"
-        )
 </template>
 
 <script>
